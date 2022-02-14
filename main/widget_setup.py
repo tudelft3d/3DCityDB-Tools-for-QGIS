a8596c3263b23adf7bb097772e86d9c2f2dc1e0d
Konstantinos-Pantelios <konstantinospantelios@yahoo.com>
2022-02-14 08:16:01 +0100

    Implement settings' tab buttons (all except individual schema uninstallation

--- main/widget_setup.py
+++ main/widget_setup.py
@@ -5,11 +5,10 @@ from .connection_tab import *
 from .functions import *
 from .threads import *
 from .import_tab import *
-from qgis.PyQt.QtCore import QThread
-from qgis.utils import iface
+from .widget_reset import *
 
 
-
+### Connection tab
 def cbxExistingConnection_setup(dbLoader):
     selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
 
@@ -33,13 +32,13 @@ def cbxExistingConnection_setup(dbLoader):
     dbLoader.dlg.lblInstall_out.clear()
 
     ### User Type groupbox
-    deselect_radiobtn_group(dbLoader)
+    reset_gbxUserType(dbLoader)
     dbLoader.dlg.gbxUserType.setDisabled(True)
 
     if dbLoader.conn: dbLoader.conn.close()
 
-    reset_importTab(dbLoader)
-    reset_settingsTab(dbLoader)
+    reset_tabImport(dbLoader)
+    reset_tabSettings(dbLoader)
 
     dbLoader.connection_status={'ConnectedToDB':False,'ServerVersion':False,'UserPrivileges':False,'3DCityDBVersion':False,'Install':False}
 
@@ -54,15 +53,15 @@ def btnConnectToDB_setup(dbLoader):
         
         ##Show successful database name connection and server version
         dbLoader.dlg.lblConnectedToDB_out.setText(success_html.format(selected_db.database_name))
-        dbLoader.connection_status['ConnectedToDB']=True
+        selected_db.green_connection=True
         dbLoader.dlg.lblServerVersion_out.setText(success_html.format(selected_db.s_version))
-        dbLoader.connection_status['ServerVersion']=True 
+        selected_db.green_s_version=True
 
         if is_3dcitydb(dbLoader):
-            dbLoader.connection_status['3DCityDBVersion']=True
+            selected_db.green_c_verison=True
             dbLoader.dlg.lbl3DCityDBVersion_out.setText(success_html.format(selected_db.c_version))
         else: 
-            dbLoader.connection_status['3DCityDBVersion']=False
+            selected_db.green_c_verison=False
             dbLoader.dlg.lbl3DCityDBVersion_out.setText(failure_html.format(selected_db.database_name+' DOES NOT have 3DCityDB installed.'))
 
 
@@ -76,12 +75,11 @@ def btnConnectToDB_setup(dbLoader):
 
     else: 
         dbLoader.dlg.lblConnectedToDB_out.setText(failure_html.format('Unsucessful connection'))
-        dbLoader.connection_status['ConnectedToDB']=False
+        selected_db.green_connection=False
         dbLoader.dlg.lblServerVersion_out.setText(failure_html.format('')) 
-        dbLoader.connection_status['ServerVersion']=False     
+        selected_db.green_s_version=False     
         dbLoader.dlg.cbxSchema.setDisabled(True)
         dbLoader.dlg.lblSchema.setDisabled(True)
-
 
 def cbxSchema_setup(dbLoader):
     selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
@@ -92,32 +90,33 @@ def cbxSchema_setup(dbLoader):
     ### Connection Status groupbox
     dbLoader.dlg.lblInstall.setText(f"Installation for {selected_schema}:")
     dbLoader.dlg.lblUserPrivileges_out.clear()
+
+   
     
     #Catch and show errors
     privileges_dict=table_privileges(dbLoader)
     if not privileges_dict: 
         dbLoader.dlg.lblInstall_out.clear()
-        dbLoader.connection_status['UserPrivileges']=False
+        selected_db.green_privileges=False
         return dbLoader.dlg.lblUserPrivileges_out.setText(failure_html.format('An error occured assesing privileges. See log'))
 
     #Show user privilages
     dbLoader.availiable_privileges = true_privileges(privileges_dict)
     if len(privileges_dict)==len(privileges_dict):
         dbLoader.dlg.lblUserPrivileges_out.setText(success_html.format(dbLoader.availiable_privileges))
-        dbLoader.connection_status['UserPrivileges']=True
+        selected_db.green_privileges=True
     elif len(privileges_dict)==0:
         dbLoader.dlg.lblUserPrivileges_out.setText(failure_html.format(dbLoader.availiable_privileges))
-        dbLoader.connection_status['UserPrivileges']=False
+        selected_db.green_privileges=False
     else: dbLoader.dlg.lblUserPrivileges_out.setText(crit_warning_html.format(dbLoader.availiable_privileges))
     
-
-    #Check for schema installation
+     #Check for schema installation
     dbLoader.dlg.lblInstall_out.clear()
     has_qgispkg= has_qgis_pkg(dbLoader)
     if not has_qgispkg:
         dbLoader.dlg.lblInstall_out.setText(crit_warning_html.format('qgis_pkg is not installed!\n\tRequires installation!'))
-        dbLoader.connection_status['Install']=False
-        return installation_query(dbLoader,f"Database '{selected_db.database_name}' requires 'qgis_pkg' to be installed with contents mapping '{selected_schema}' schema.\nDo you want to proceed?")
+        selected_db.green_installation=False
+        return installation_query(dbLoader,f"Database '{selected_db.database_name}' requires 'qgis_pkg' to be installed with contents mapping '{selected_schema}' schema.\nDo you want to proceed?",origin=dbLoader.dlg.lblInstallLoadingCon)
     else:
         selected_db.has_installation = True
         qgispkg_has_views= has_schema_views(dbLoader,selected_schema)
@@ -125,38 +124,78 @@ def cbxSchema_setup(dbLoader):
 
         if qgispkg_has_views:
             dbLoader.dlg.lblInstall_out.setText(success_html.format('qgis_pkg is already installed!'))
-            dbLoader.connection_status['Install']=True
+            selected_db.green_installation=True
         elif has_qgispkg and not qgispkg_has_views: 
             dbLoader.dlg.lblInstall_out.setText(crit_warning_html.format(f'qgis_pkg is already installed but NOT for {selected_schema}!\n\tRequires installation!'))
-            dbLoader.connection_status['Install']=False
-            return installation_query(dbLoader,f"'qgis_pkg' needs to be enhanced with contents mapping '{selected_schema}' schema.\nDo you want to proceed?")
+            selected_db.green_installation=False
+            return False#installation_query(dbLoader,f"'qgis_pkg' needs to be enhanced with contents mapping '{selected_schema}' schema.\nDo you want to proceed?")
 
+    
     return True
 
 def gbxUserType_setup(dbLoader,user_type):
     print(f'I am {user_type}')
     selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
     selected_schema=dbLoader.dlg.cbxSchema.currentText()
-    reset_importTab(dbLoader)
+    reset_tabImport(dbLoader)
     
     dbLoader.dlg.tabImport.setDisabled(False)
     dbLoader.dlg.lblDbSchema.setText(dbLoader.dlg.lblDbSchema.init_text.format(Database=selected_db.database_name,Schema=selected_schema))
     dbLoader.dlg.lblDbSchema.setDisabled(False)
-
-    settingsTab_setup(dbLoader,user_type)
+    
+    dbLoader.dlg.tabSettings.setDisabled(False)
+    tabSettings_setup(dbLoader,user_type)
     
 
 
     dbLoader.dlg.wdgMain.setCurrentIndex(1)
 
-def settingsTab_setup(dbLoader,user_type):
+### Import tab
+def btnCityExtents_setup(dbLoader):
+    cur = dbLoader.conn.cursor()
+    cur.execute("""SELECT ST_AsText(ST_SetSRID(ST_Extent(envelope),28992)),'' FROM citydb.cityobject""")
+    extents,empty = cur.fetchone()
+    cur.close()
+    crs =dbLoader.iface.mapCanvas().mapSettings().destinationCrs().authid()
+    qgis_extent=QgsRectangle.fromWkt(extents)
+    dbLoader.dlg.qgbxExtent.setOutputExtentFromUser(qgis_extent,QgsCoordinateReferenceSystem(crs))
+    
+def qgbxExtent_setup(dbLoader):
+    dbLoader.dlg.cbxModule.clear()
+    dbLoader.dlg.gbxParameters.setDisabled(False)
+    fill_module_box(dbLoader)
+
+def cbxModule_setup(dbLoader):
+    dbLoader.dlg.cbxLod.clear()
+    dbLoader.dlg.cbxLod.setDisabled(False)
+    fill_lod_box(dbLoader)
+
+def cbxLod_setup(dbLoader):
+    dbLoader.dlg.gbxFeatures.setDisabled(False)
+    dbLoader.dlg.ccbxFeatures.clear()
+    dbLoader.dlg.ccbxFeatures.setDefaultText("Select availiable features to import")
+    fill_features_box(dbLoader)
+             
+def ccbxFeatures_setup(dbLoader):
+
+    checked_views= dbLoader.dlg.ccbxFeatures.checkedItems()
+
+    if checked_views:
+        dbLoader.dlg.btnImport.setDisabled(False)
+        dbLoader.dlg.btnImport.setText(dbLoader.dlg.btnImport.init_text.format(num=len(checked_views)))
+    else: 
+        dbLoader.dlg.btnImport.setText(dbLoader.dlg.btnImport.init_text)
+        dbLoader.dlg.btnImport.setDisabled(True)
+
+### Settings tab
+def tabSettings_setup(dbLoader,user_type):
     selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
     selected_schema=dbLoader.dlg.cbxSchema.currentText()
 
 
 
     if user_type=='Viewer':        
-        reset_settingsTab(dbLoader)
+        reset_tabSettings(dbLoader)
 
     elif user_type=='Editor':
         dbLoader.dlg.btnInstallDB.setText(dbLoader.dlg.btnInstallDB.init_text.format(DB=selected_db.database_name,SC=selected_schema))
@@ -171,26 +210,7 @@ def settingsTab_setup(dbLoader,user_type):
         dbLoader.dlg.btnRefreshViews.setText(dbLoader.dlg.btnRefreshViews.init_text.format(DB=selected_db.database_name,SC=selected_schema))
         dbLoader.dlg.btnRefreshViews.setDisabled(False)
 
-def reset_importTab(dbLoader):
-    selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
-    selected_schema=dbLoader.dlg.cbxSchema.currentText()
-
-    dbLoader.dlg.tabImport.setDisabled(True)
-    dbLoader.dlg.lblDbSchema.setText(dbLoader.dlg.lblDbSchema.init_text)
-
-def reset_settingsTab(dbLoader):
-
-    dbLoader.dlg.btnInstallDB.setDisabled(True)
-    dbLoader.dlg.btnInstallDB.setText(dbLoader.dlg.btnInstallDB.init_text)
-    dbLoader.dlg.btnUnInstallDB.setDisabled(True)
-    dbLoader.dlg.btnUnInstallDB.setText(dbLoader.dlg.btnUnInstallDB.init_text)
-    dbLoader.dlg.btnClearDB.setDisabled(True)
-    dbLoader.dlg.btnClearDB.setText(dbLoader.dlg.btnClearDB.init_text)
-    dbLoader.dlg.btnRefreshViews.setDisabled(True)
-    dbLoader.dlg.btnRefreshViews.setText(dbLoader.dlg.btnRefreshViews.init_text)
-    #dbLoader.dlg.gbxExtent.setDisabled(True)
-    #dbLoader.dlg.qgbxExtent.c
-    
+    dbLoader.dlg.gbxExtent.setDisabled(False)
 
 def btnRefreshViews_setup(dbLoader):
     cur = dbLoader.conn.cursor()
@@ -199,62 +219,13 @@ def btnRefreshViews_setup(dbLoader):
     
     if res == 16384: #YES   
         refresh_views_thread(dbLoader,cursor=cur)
-
         
 def btnInstallDB_setup(dbLoader):
-    selected_db=dbLoader.dlg.cbxExistingConnection.currentData()
+    installation_query(dbLoader, "This is going to take a while! Do you want to proceed?",origin=dbLoader.dlg.lblLoadingInstall)
 
-    upd_conn_file(dbLoader) #Prepares installation scripts with the connection parameters 
-    
-    success = install(dbLoader, origin=dbLoader.dlg.lblLoadingInstall)
-    if success: 
-        selected_db.has_installation = True
-        dbLoader.connection_status['Install']=True
-        if not dbLoader.plugin_package in dbLoader.schemas: dbLoader.schemas.append(dbLoader.plugin_package)
-    else:    
-        dbLoader.connection_status['Install']=False
-        dbLoader.dlg.btnClearDB.setDisabled(False)
-        dbLoader.dlg.btnClearDB.setDefault(True)
-        dbLoader.dlg.btnClearDB.setText(f'Clear corrupted installation!')  
-        dbLoader.dlg.wdgMain.setCurrentIndex(2)            
-
-def btnCityExtents_setup(dbLoader):
-    cur = dbLoader.conn.cursor()
-    cur.execute("""SELECT ST_AsText(ST_SetSRID(ST_Extent(envelope),28992)),'' FROM citydb.cityobject""")
-    extents,empty = cur.fetchone()
-    cur.close()
-    crs =dbLoader.iface.mapCanvas().mapSettings().destinationCrs().authid()
-    qgis_extent=QgsRectangle.fromWkt(extents)
-    dbLoader.dlg.qgbxExtent.setOutputExtentFromUser(qgis_extent,QgsCoordinateReferenceSystem(crs))
-
-
-def qgbxExtent_setup(dbLoader):
-    dbLoader.dlg.cbxModule.clear()
-    dbLoader.dlg.gbxParameters.setDisabled(False)
-    fill_module_box(dbLoader)
-
-    #dbLoader.dlg.btnImport.setDisabled(True)
-
-
-def cbxModule_setup(dbLoader):
-    dbLoader.dlg.cbxLod.clear()
-    dbLoader.dlg.cbxLod.setDisabled(False)
-    fill_lod_box(dbLoader)
-
-def cbxLod_setup(dbLoader):
-    dbLoader.dlg.gbxFeatures.setDisabled(False)
-    dbLoader.dlg.ccbxFeatures.clear()
-    dbLoader.dlg.ccbxFeatures.setDefaultText("Select availiable features to import")
-    fill_features_box(dbLoader)
-    
-            
-def ccbxFeatures_setup(dbLoader):
-
-    checked_views= dbLoader.dlg.ccbxFeatures.checkedItems()
-
-    if checked_views:
-        dbLoader.dlg.btnImport.setDisabled(False)
-        dbLoader.dlg.btnImport.setText(dbLoader.dlg.btnImport.init_text.format(num=len(checked_views)))
-    else: 
-        dbLoader.dlg.btnImport.setText(dbLoader.dlg.btnImport.init_text)
-        dbLoader.dlg.btnImport.setDisabled(True)
+def btnClearDB_setup(dbLoader):
+    uninstall_pkg(dbLoader)
+    reset_tabImport(dbLoader)
+    reset_tabConnection(dbLoader)
+    dbLoader.dlg.btnClearDB.setDisabled(True)
+    dbLoader.dlg.btnClearDB.setText(dbLoader.dlg.btnClearDB.init_text)
