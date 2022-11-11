06894e2e49a092a62bd90bde76700014dd8631df
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2022-11-11 01:30:08 +0100

    Use shared functions

--- cdb4/shared/functions/sql.py
+++ cdb4/shared/functions/sql.py
@@ -3,28 +3,29 @@
 These functions are responsible to communicate and fetch data from
 the database with sql queries or sql function calls.
 """
-from qgis.core import Qgis, QgsMessageLog
 import psycopg2
-from ....cdb_loader import CDBLoader # Used only to add the type of the function parameters
-from ... import cdb4_constants as c
+from qgis.core import Qgis, QgsMessageLog
 
-FILE_LOCATION = c.get_file_relative_path(file=__file__)
+from ....cdb_loader import CDBLoader  # Used only to add the type of the function parameters
+from . import general_functions as gen_f
+
+FILE_LOCATION = gen_f.get_file_relative_path(file=__file__)
 
 def fetch_3dcitydb_version(cdbLoader: CDBLoader) -> str:
     """SQL query that reads and retrieves the 3DCityDB version.
+
     *   :returns: 3DCityDB version.
         :rtype: str
     """
-    version: str
     try:
         with cdbLoader.conn.cursor() as cur:
             cur.execute(query="""SELECT version FROM citydb_pkg.citydb_version();""")
-            version = cur.fetchone()[0] # Tuple has trailing comma.
+            version: str = cur.fetchone()[0] # Tuple has trailing comma.
         cdbLoader.conn.commit()
         return version
 
     except (Exception, psycopg2.Error) as error:
-        c.critical_log(
+        gen_f.critical_log(
             func=fetch_3dcitydb_version,
             location=FILE_LOCATION,
             header="Retrieving 3DCityDB version",
@@ -34,27 +35,26 @@ def fetch_3dcitydb_version(cdbLoader: CDBLoader) -> str:
 def exec_create_qgis_usr_schema_name(cdbLoader: CDBLoader, usr_name: str = None) -> str:
     """Calls the qgis_pkg function that derives the name of the usr_schema from the usr_name.
     """
-    usr_schema: str
     if usr_name is None:
-        usr_name = cdbLoader.DB.username
+        usr_name: str = cdbLoader.DB.username
 
     try:
         with cdbLoader.conn.cursor() as cur:
             cur.callproc(f"""{cdbLoader.QGIS_PKG_SCHEMA}.create_qgis_usr_schema_name""", [usr_name])
-            usr_schema = cur.fetchone()[0] # Trailing comma
+            usr_schema: str = cur.fetchone()[0] # Trailing comma
         cdbLoader.USR_SCHEMA = usr_schema
         cdbLoader.conn.commit()
         return usr_schema
 
     except (Exception, psycopg2.Error) as error:
-        c.critical_log(
+        gen_f.critical_log(
             func=exec_create_qgis_usr_schema_name,
             location=FILE_LOCATION,
             header=f"Deriving user schema name for user {usr_name}",
             error=error)
         cdbLoader.conn.rollback()
 
-def is_qgis_pkg_intalled(cdbLoader: CDBLoader) -> bool:
+def is_qgis_pkg_installed(cdbLoader: CDBLoader) -> bool:
     """SQL query that searches for schema 'qgis_pkg' in the current database
 
     *   :returns: Search result
@@ -72,8 +72,8 @@ def is_qgis_pkg_intalled(cdbLoader: CDBLoader) -> bool:
         return False
 
     except (Exception, psycopg2.Error) as error:
-        c.critical_log(
-            func=is_qgis_pkg_intalled,
+        gen_f.critical_log(
+            func=is_qgis_pkg_installed,
             location=FILE_LOCATION,
             header=f"Searching for schema {cdbLoader.QGIS_PKG_SCHEMA} in current database",
             error=error)
@@ -93,7 +93,7 @@ def exec_qgis_pkg_version(cdbLoader: CDBLoader) -> tuple:
         return version
 
     except (Exception, psycopg2.Error) as error:
-        c.critical_log(
+        gen_f.critical_log(
             func=exec_qgis_pkg_version,
             location=FILE_LOCATION,
             header=f"Retrieving {cdbLoader.QGIS_PKG_SCHEMA} version",
@@ -118,7 +118,7 @@ def is_usr_schema_installed(cdbLoader: CDBLoader) -> bool:
         return False
 
     except (Exception, psycopg2.Error) as error:
-        c.critical_log(
+        gen_f.critical_log(
             func=is_usr_schema_installed,
             location=FILE_LOCATION,
             header=f"Checking whether schema '{cdbLoader.USR_SCHEMA}' is installed in current database",
@@ -133,7 +133,7 @@ def exec_list_cdb_schemas_all(cdbLoader: CDBLoader, only_non_empty: bool = False
     """
     try:
         with cdbLoader.conn.cursor() as cur:
-            cur.callproc(f"""{cdbLoader.QGIS_PKG_SCHEMA}.list_cdb_schemas_new""", [only_non_empty])
+            cur.callproc(f"""{cdbLoader.QGIS_PKG_SCHEMA}.list_cdb_schemas""", [only_non_empty])
             result = cur.fetchall()
         cdbLoader.conn.commit()
         schema_names = tuple(zip(*result))[0] # trailing comma
