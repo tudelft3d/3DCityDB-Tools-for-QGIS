f8c31021a490bbab38122fcbd21e01432fffffa9
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2022-11-11 01:31:02 +0100

    Update tab_layers_widget_functions.py

--- cdb4/gui_usr/functions/tab_layers_widget_functions.py
+++ cdb4/gui_usr/functions/tab_layers_widget_functions.py
@@ -1,10 +1,7 @@
-"""This module contains reset functions for each QT widget in the GUI of the
-plugin.
+"""This module contains reset functions for each QT widget in the GUI of the plugin.
 
 The logic behind all of the functions is to reset widgets as individual
 objects or as block of objects, depending on the needs.
-
-These function help declutter the widget_setup.py from repetitive code.
 
 The reset functions consist of clearing text or changed text to original state,
 clearing widget items or selections and deactivating widgets.
@@ -12,83 +9,55 @@ clearing widget items or selections and deactivating widgets.
 
 #from qgis.core import Qgis, QgsMessageLog 
 from qgis.PyQt.QtCore import Qt
+from qgis.core import QgsRectangle
 
 from ....cdb_loader import CDBLoader # Used only to add the type of the function parameters
-from ... import cdb4_constants as c
+from ...shared.functions import general_functions as gen_f
 from . import canvas
 
-FILE_LOCATION = c.get_file_relative_path(file=__file__)
+FILE_LOCATION = gen_f.get_file_relative_path(file=__file__)
 
 ####################################################
 ## Setup widget functions for 'Layer' tab
 ####################################################
 
 # In 'Basemap (OMS)' groupBox.
-def gbxBasemap_setup(cdbLoader: CDBLoader, canvas_widget) ->  None:
-    """Function to setup the 'Basemap' groupbox.
-    It uses an additional canvas instance to store an OSM map
+def gbxBasemap_setup(cdbLoader: CDBLoader) ->  None:
+    """Function to setup the 'Basemap' groupbox. It uses an additional canvas instance to store an OSM map
     from which extents can be extracted for further spatial queries.
-
-    The basemap is zoomed-in the city model's extents.
-
-    (in 'Layers' tab)
-
-    .. While this function is almost identical to the one handling the
-    canvas in the User Connection tab, I think, it is prudent to duplicate
-    for better debugging comprehension.
+    The basemap is zoomed-in to the city model's extents (in 'Layers' tab)
     """
-
-    # Put extents coordinates into the widget.
-    #cdbLoader.usr_dlg.qgbxExtentsC.setOutputExtentFromUser(cdbLoader.CDB_SCHEMA_EXTENTS, cdbLoader.CRS)
-
-    # Set basemap.
-    canvas.canvas_setup(
-        cdbLoader,
-        canvas=canvas_widget,
-        #extents=cdbLoader.CDB_SCHEMA_EXTENTS,
-        extents=cdbLoader.LAYER_EXTENTS,        
-        crs=cdbLoader.CRS,
-        clear=False)
+    # Set basemap of the layer tab.
+    canvas.canvas_setup(cdbLoader, canvas=cdbLoader.CANVAS, extents=cdbLoader.LAYER_EXTENTS_RED, crs=cdbLoader.CRS, clear=False)
 
     # Draw rubberband for extents of selected {cdb_schema}.
-    canvas.insert_rubber_band(
-        band=cdbLoader.RUBBER_SCHEMA,
-        extents=cdbLoader.CDB_SCHEMA_EXTENTS,
-        crs=cdbLoader.CRS,
-        width=3,
-        color=Qt.blue)
+    canvas.insert_rubber_band(band=cdbLoader.RUBBER_CDB_SCHEMA_BLUE, extents=cdbLoader.CDB_SCHEMA_EXTENTS_BLUE, crs=cdbLoader.CRS, width=3, color=Qt.blue)
 
     # Draw rubberband for extents of materialized views in selected {cdb_schema}.
-    canvas.insert_rubber_band(
-        band=cdbLoader.RUBBER_LAYERS,
-        extents=cdbLoader.LAYER_EXTENTS,
-        crs=cdbLoader.CRS,
-        width=2,
-        color=Qt.red)
+    canvas.insert_rubber_band(band=cdbLoader.RUBBER_LAYERS_RED, extents=cdbLoader.LAYER_EXTENTS_RED, crs=cdbLoader.CRS, width=2, color=Qt.red)
 
-    # Zoom to layer extents.
-    canvas_widget.zoomToFeatureExtent(cdbLoader.LAYER_EXTENTS)
+    # Zoom to layer extents (red box).
+    # PROBLEM: The zoom operation enlarges automatically the size of the extents in the variable passed.
+    # WORKAROUND: Therefore we decouple it from the global variable by transforming to/from WKT
+    canvas.zoom_to_extents(canvas=cdbLoader.CANVAS, extents=cdbLoader.LAYER_EXTENTS_RED)
+    #temp_extents_wkt: str = cdbLoader.LAYER_EXTENTS_RED.asWktPolygon()
+    #temp_rectangle: QgsRectangle = QgsRectangle.fromWkt(temp_extents_wkt)
+    #cdbLoader.CANVAS.zoomToFeatureExtent(temp_rectangle)
 
-    # Draw the extents in the canvas
-    # Create polygon rubber band corresponding to the extents
-    canvas.insert_rubber_band(
-        band=cdbLoader.RUBBER_USER,
-        extents=cdbLoader.CURRENT_EXTENTS,
-        crs=cdbLoader.CRS,
-        width=1,
-        color=Qt.green)
+    # Create polygon rubber band corresponding to the extents (the green one)
+    canvas.insert_rubber_band(band=cdbLoader.RUBBER_QGIS_GREEN, extents=cdbLoader.CURRENT_EXTENTS, crs=cdbLoader.CRS, width=1, color=Qt.green)
 
 
 ####################################################
 ## Reset widget functions for 'Layer' tab
 ####################################################
 
-# Import tab # TODO: create reset function for the basemap
 def tabLayers_reset(cdbLoader: CDBLoader) -> None:
     """Function to reset the 'Import' tab.
     Resets: gbxAvailableL, gbxLayerSelection, gbxExtent and lblInfoText.
     """
-    cdbLoader.usr_dlg.tabLayers.setDisabled(True)
+    dlg = cdbLoader.usr_dlg
+    dlg.tabLayers.setDisabled(True)
     gbxAvailableL_reset(cdbLoader)
     gbxLayerSelection_reset(cdbLoader)
     gbxBasemap_reset(cdbLoader)
@@ -96,34 +65,38 @@ def tabLayers_reset(cdbLoader: CDBLoader) -> None:
 
 
 def lblInfoText_reset(cdbLoader: CDBLoader) -> None:
-    """Function to reset the 'DB and Schema' label (in Layers tab)."""
-
-    cdbLoader.usr_dlg.lblInfoText.setText(cdbLoader.usr_dlg.lblInfoText.init_text)
-    cdbLoader.usr_dlg.lblInfoText.setDisabled(True)
+    """Function to reset the 'DB and Schema' label (in Layers tab).
+    """
+    dlg = cdbLoader.usr_dlg
+    dlg.lblInfoText.setText(dlg.lblInfoText.init_text)
+    dlg.lblInfoText.setDisabled(True)
 
 
 def gbxBasemap_reset(cdbLoader: CDBLoader) -> None:
-    """Function to reset the 'Extents' groupbox (in Layers tab)."""
-
-    cdbLoader.usr_dlg.qgbxExtents.setDisabled(True)
+    """Function to reset the 'Extents' groupbox (in Layers tab).
+    """
+    dlg = cdbLoader.usr_dlg
+    dlg.qgbxExtents.setDisabled(True)
 
     # Remove extent rubber bands.
-    cdbLoader.RUBBER_SCHEMA.reset()
-    cdbLoader.RUBBER_LAYERS.reset()
-    cdbLoader.RUBBER_USER.reset()
+    cdbLoader.RUBBER_CDB_SCHEMA_BLUE.reset()
+    cdbLoader.RUBBER_LAYERS_RED.reset()
+    cdbLoader.RUBBER_QGIS_GREEN.reset()
 
 
 def gbxLayerSelection_reset(cdbLoader: CDBLoader) -> None:
-    """Function to reset the 'Parameters' group box (in Layers tab)."""
-
-    cdbLoader.usr_dlg.gbxLayerSelection.setDisabled(True)
-    cdbLoader.usr_dlg.cbxFeatureType.clear()
-    cdbLoader.usr_dlg.cbxLod.clear()
+    """Function to reset the 'Parameters' group box (in Layers tab).
+    """
+    dlg = cdbLoader.usr_dlg
+    dlg.gbxLayerSelection.setDisabled(True)
+    dlg.cbxFeatureType.clear()
+    dlg.cbxLod.clear()
 
 
 def gbxAvailableL_reset(cdbLoader: CDBLoader) -> None:
-    """Function to reset the 'Features to Import' group box (in Layers tab)."""
-
-    cdbLoader.usr_dlg.gbxAvailableL.setDisabled(True)
-    cdbLoader.usr_dlg.ccbxFeatures.clear()
+    """Function to reset the 'Features to Import' group box (in Layers tab).
+    """
+    dlg = cdbLoader.usr_dlg
+    dlg.gbxAvailableL.setDisabled(True)
+    dlg.ccbxFeatures.clear()
 
