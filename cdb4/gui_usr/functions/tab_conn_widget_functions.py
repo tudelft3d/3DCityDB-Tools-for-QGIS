899017c0569a0d334f55251ec92e016e997b6b11
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2022-11-11 01:31:08 +0100

    Update tab_conn_widget_functions.py

--- cdb4/gui_usr/functions/tab_conn_widget_functions.py
+++ cdb4/gui_usr/functions/tab_conn_widget_functions.py
@@ -16,85 +16,78 @@ import psycopg2
 
 from ....cdb_loader import CDBLoader # Used only to add the type of the function parameters
 
+from ...shared.functions import general_functions as gen_f
 from ... import cdb4_constants as c
 
 from . import canvas, sql
 
-FILE_LOCATION = c.get_file_relative_path(file=__file__)
+FILE_LOCATION = gen_f.get_file_relative_path(file=__file__)
 
 ####################################################
 ## Setup widget functions for 'User Connection' tab
 ####################################################
 
 # In 'Basemap (OMS)' groupBox.
-def gbxBasemapC_setup(cdbLoader: CDBLoader, canvas_widget) ->  None:
+def gbxBasemapC_setup(cdbLoader: CDBLoader) ->  None:
     """Function to setup the 'Basemap' groupbox.
     It uses an additional canvas instance to store an OSM map
     from which extents can be extracted for further spatial queries.
 
     The basemap is zoomed-in to the city model's extents.
     """
-
     try:
-        extents_exist = False
+        extents_exist: bool = False
+
         while not extents_exist:
 
             # Get the extents stored in server.
-            extents = sql.fetch_extents(
-                cdbLoader,
-                usr_schema=cdbLoader.USR_SCHEMA,
-                cdb_schema=cdbLoader.CDB_SCHEMA,
-                ext_type=c.CDB_SCHEMA_EXT_TYPE)
+            cdb_extents: str = sql.fetch_precomputed_extents(cdbLoader, usr_schema=cdbLoader.USR_SCHEMA, cdb_schema=cdbLoader.CDB_SCHEMA, ext_type=c.CDB_SCHEMA_EXT_TYPE)
 
-            # Extents might be None (not computed yet).
-            if extents:
+            # Extents could be None (not computed yet). In the case, see the else
+            if cdb_extents:
                 extents_exist = True
+                cdb_extents_qgs_rect: QgsRectangle = QgsRectangle.fromWkt(cdb_extents)
 
-                # Get the crs stored in the selected {cdb_schema}
-                crs = sql.fetch_crs(cdbLoader)
-
+                # Get the crs_id stored in the selected {cdb_schema}
+                srid_id: int = sql.fetch_srid_id(cdbLoader)
                 # Format CRS variable as QGIS Epsg code.
-                crs = ":".join(["EPSG",str(crs)]) # e.g. EPSG:28992
+                crs: str = ":".join(["EPSG",str(srid_id)]) # e.g. EPSG:28992
+
+                # Assign the crs into the plugin variable
                 cdbLoader.CRS = QgsCoordinateReferenceSystem(crs)
+                # Store the extents into plugin variables.
+                cdbLoader.CURRENT_EXTENTS = cdb_extents_qgs_rect
+                cdbLoader.CDB_SCHEMA_EXTENTS_BLUE = cdb_extents_qgs_rect
 
-                # Store extents into plugin variables.
-                cdbLoader.CURRENT_EXTENTS = QgsRectangle.fromWkt(extents)
-                cdbLoader.CDB_SCHEMA_EXTENTS = QgsRectangle.fromWkt(extents)
+                # Draw the cdb extents in the canvas
+                # First, create polygon rubber band corresponding to the cdb_schema extents
+                canvas.insert_rubber_band(band=cdbLoader.RUBBER_CDB_SCHEMA_BLUE_C, extents=cdbLoader.CDB_SCHEMA_EXTENTS_BLUE, crs=cdbLoader.CRS, width=3, color=Qt.blue)
 
-                # # Draw the extents in the canvas
-                # # Create polygon rubber band corresponding to the extents
-                canvas.insert_rubber_band(
-                    band=cdbLoader.RUBBER_SCHEMA_C,
-                    extents=cdbLoader.CDB_SCHEMA_EXTENTS,
-                    crs=cdbLoader.CRS,
-                    width=3,
-                    color=Qt.blue)
-                
-                # Update canvas with schema extents and crs
-                canvas.canvas_setup(
-                    cdbLoader,
-                    canvas=canvas_widget,
-                    extents=cdbLoader.CDB_SCHEMA_EXTENTS,
-                    crs=cdbLoader.CRS)
+                # Then update canvas with cdb_schema extents and crs
+                canvas.canvas_setup(cdbLoader, canvas=cdbLoader.CANVAS_C, extents=cdbLoader.CDB_SCHEMA_EXTENTS_BLUE, crs=cdbLoader.CRS)
 
-                # Put extents coordinates into the widget.
-                #dbLoader.usr_dlg.qgbxExtentsC.setOutputExtentFromUser(dbLoader.CDB_SCHEMA_EXTENTS,dbLoader.CRS)
+                # Zoom to the cdb_schema extents (blue box)
+                # PROBLEM: The zoom operation enlarges automatically the size of the extents in the variable passed.
+                # WORKAROUND: Therefore we decouple it from the global variable by transforming to/from WKT
+                canvas.zoom_to_extents(canvas=cdbLoader.CANVAS_C, extents=cdbLoader.CDB_SCHEMA_EXTENTS_BLUE)
+                #temp_extents_wkt: str = cdbLoader.CDB_SCHEMA_EXTENTS_BLUE.asWktPolygon()
+                #temp_rectangle: QgsRectangle = QgsRectangle.fromWkt(temp_extents_wkt)
+                #cdbLoader.CANVAS_C.zoomToFeatureExtent(temp_rectangle)
 
-                # Zoom to these extents.
-                canvas_widget.zoomToFeatureExtent(cdbLoader.CDB_SCHEMA_EXTENTS)
-            
-            else: # Compute the extents.
-                sql.exec_compute_schema_extents(cdbLoader)
-                if not sql.fetch_extents(
-                            cdbLoader,
-                            usr_schema=cdbLoader.USR_SCHEMA,
-                            cdb_schema=cdbLoader.CDB_SCHEMA,
-                            ext_type=c.CDB_SCHEMA_EXT_TYPE):
+            else: 
+                # There are no precomputed extents for the cdb_schema, so compute them "for real" (bbox of all cityobjects)'.
+                # This function automatically upsert the bbox to the table of the precomputed extents in the usr_schema
+                sql.exec_compute_cdb_schema_extents(cdbLoader)
+
+                # Check that it has been actually done, i.e. the cdb_extents are now available to be read in the next iteration.
+                cdb_extents_test: str = sql.fetch_precomputed_extents(
+                    cdbLoader, usr_schema=cdbLoader.USR_SCHEMA, cdb_schema=cdbLoader.CDB_SCHEMA, ext_type=c.CDB_SCHEMA_EXT_TYPE)
+                if not cdb_extents_test:
                     raise Exception('Function {c.MAIN_PKG_NAME}.compute_schema_extent() returned: None')
 
     except (Exception, psycopg2.Error) as error:
         # Send error to QGIS Message Log panel.
-        c.critical_log(
+        gen_f.critical_log(
             func=gbxBasemapC_setup,
             location=FILE_LOCATION,
             header="Retrieving extents",
@@ -156,9 +149,9 @@ def gbxBasemapC_reset(cdbLoader: CDBLoader) -> None:
     cdbLoader.usr_dlg.btnCityExtentsC.setText(cdbLoader.usr_dlg.btnCityExtentsC.init_text)
 
     # Remove extent rubber bands.
-    cdbLoader.RUBBER_SCHEMA_C.reset()
-    cdbLoader.RUBBER_LAYERS_C.reset()
-    cdbLoader.RUBBER_USER.reset()
+    cdbLoader.RUBBER_CDB_SCHEMA_BLUE_C.reset()
+    cdbLoader.RUBBER_LAYERS_RED_C.reset()
+    cdbLoader.RUBBER_QGIS_GREEN.reset()
 
     # Clear map registry from OSM layers.
     registryLayers = [i.id() for i in QgsProject.instance().mapLayers().values() if c.OSM_NAME == i.name()]
