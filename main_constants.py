123be293469968e9e6b5b5ecc8e07ba1b379a655
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2022-11-11 01:23:09 +0100

    Add plugin version variables

--- main_constants.py
+++ main_constants.py
@@ -1,12 +1,14 @@
-"""This module contains all hardcoded or constant elements that are
-used within the plugin
+"""This module contains hardcoded variables that are used within the plugin
 """
-
 import os.path
 
 # Names
 PLUGIN_NAME: str = "3DCityDB-Loader"
 PLUGIN_NAME_ADMIN: str = " ".join([PLUGIN_NAME, "(Administration)"])
+
+PLUGIN_VERSION_MAJOR: int = 0
+PLUGIN_VERSION_MINOR: int = 6
+PLUGIN_VERSION_REV:   int = 0
 
 QGIS_PKG_SCHEMA: str = "qgis_pkg"
 USR_SCHEMA: str = "qgis_{user}"
