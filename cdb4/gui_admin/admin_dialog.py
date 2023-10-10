de2f2bc14a5e00c60431327098d1e7237b4934c8
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2023-10-10 12:25:06 +0200

    Updates to Admin Dialog

--- cdb4/gui_admin/admin_dialog.py
+++ cdb4/gui_admin/admin_dialog.py
@@ -15,10 +15,19 @@
 
 /***************************************************************************
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+   Copyright 2021 Giorgio Agugiaro, Konstantinos Pantelios
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  *                                                                         *
  ***************************************************************************/
 """
@@ -62,7 +71,6 @@ class CDB4AdminDialog(QtWidgets.QDialog, FORM_CLASS):
         # Set up the user interface from Designer through FORM_CLASS.
         # After self.setupUi() you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots
-
         self.setupUi(self)
 
         ############################################################
