3cedd8a42ac23e0d9493fb791df93a3cf7304f75
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2023-11-12 19:48:15 +0100

    Update loader_constants.py

--- cdb4/gui_loader/loader_constants.py
+++ cdb4/gui_loader/loader_constants.py
@@ -1,9 +1,10 @@
-"""This module contains constant values that are used within the CityDB-Loader plugin for 3DCityDB v. 4.x
+"""This module contains constant values that are used within the CityDB-Tools plugin for 3DCityDB v. 4.x
 """
 import os.path
-from qgis.core import QgsCoordinateReferenceSystem, QgsRectangle
+
 from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtCore import Qt
+from qgis.core import QgsCoordinateReferenceSystem, QgsRectangle
 
 from ... import cdb_tools_main_constants as main_c
 
@@ -20,16 +21,10 @@ QML_FORM_DIR: str = "form"
 QML_SYMB_DIR: str = "symb"
 QML_3D_DIR: str = "3d"
 
-# Extent types
-CDB_SCHEMA_EXT_TYPE: str = "db_schema"
-LAYER_EXT_TYPE: str      = "m_view"
-QGIS_EXT_TYPE: str       = "qgis"
-
-# Extent types
 # Check more here: https://doc.qt.io/qt-5/qt.html#GlobalColor-enum
-CDB_EXTENTS_COLOUR:   QColor = Qt.black
+CDB_EXTENTS_COLOUR: QColor = Qt.black
 LAYER_EXTENTS_COLOUR: QColor = Qt.blue
-QGIS_EXTENTS_COLOUR:  QColor = Qt.darkGreen
+QGIS_EXTENTS_COLOUR: QColor = Qt.darkGreen
 
 # Basemaps
 GOOGLE_URL: str = "http://mt1.google.com/vt/lyrs%3Dm%26x%3D%7Bx%7D%26y%3D%7By%7D%26z%3D%7Bz%7D&"
@@ -38,17 +33,17 @@ GOOGLE_URI: str = f"type=xyz&url={GOOGLE_URL}zmax=22&zmin=0"
 OSM_NAME: str = "OSM Basemap"
 OSM_URL: str = "https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png"
 OSM_URI: str = f"type=xyz&url={OSM_URL}&zmax=22&zmin=0"
-OSM_INIT_EXTS = QgsRectangle(-14372453, -6084688, 16890255, 13952819)
-OSM_INIT_CRS = QgsCoordinateReferenceSystem("EPSG:3857")
+OSM_INIT_EXTS: QgsRectangle = QgsRectangle(-14372453, -6084688, 16890255, 13952819)
+OSM_INIT_CRS: QgsCoordinateReferenceSystem = QgsCoordinateReferenceSystem("EPSG:3857")
 
 # ToC labels and constancts 
-detail_views_group_alias: str  = "Form detail views"
+detail_views_group_alias: str = "Form detail views"
 lookup_tables_group_alias: str = "Look-up tables"
 
-generics_table: str     = "cityobject_genericattrib"
+generics_table: str = "cityobject_genericattrib"
 
 enumerations_table: str = "v_enumeration_value"
-codelists_table: str    = "v_codelist_value"
+codelists_table: str = "v_codelist_value"
 
 # Usr dialog strings
 CONN_FAIL_MSG: str = "Connection failed"
@@ -68,39 +63,39 @@ LAYER_DR_SUCC_MSG: str  = "Layers removed from schema '{sch}'"
 LAYER_DR_ERROR_MSG: str = "Error while removing layers from schema '{sch}'"
 
 # Widget initial embedded text | Note: empty spaces are for positioning.
-btnConnectToDB_t: str  = "Connect to database '{db}'"
-btnRefreshCDBExtents_t: str  = "Refresh '{sch}' extents"
-btnCityExtents_t: str  = "Set to schema '{sch}'"
-btnCreateLayers_t: str  = "Create layers for schema '{sch}'"
+btnConnectToDB_t: str = "Connect to database '{db}'"
+btnRefreshCDBExtents_t: str = "Refresh '{sch}' extents"
+btnCityExtents_t: str = "Set to schema '{sch}'"
+btnCreateLayers_t: str = "Create layers for schema '{sch}'"
 btnRefreshLayers_t: str = " Refresh layers for schema '{sch}'"
-btnDropLayers_t: str    = "Drop layers for schema '{sch}'"
-lblInfoText_t: str    = "Current database: '{db}'\nCurrent user: '{usr}'\nCurrent citydb schema: '{sch}'"
+btnDropLayers_t: str = "Drop layers for schema '{sch}'"
+lblInfoText_t: str = "Current database: '{db}'\nCurrent user: '{usr}'\nCurrent citydb schema: '{sch}'"
 btnLayerExtentsL_t: str = "Set to layer extents"
-ccbxFeatures_t: str   = "Select available features to import"
+ccbxFeatures_t: str = "Select available features to import"
 
 # Text - Messages - Log
 icon_msg_core: str = """
                 <html><head/><body><p> 
                 <img src="{image_rc}" style='vertical-align: bottom'/> 
-                <span style=" color:{color_hex};">{addtional_text}</span>
+                <span style=" color:{color_hex};">{additional_text}</span>
                 </p></body></html>
                 """
 success_html: str = icon_msg_core.format(
     image_rc=':/plugins/citydb_loader/icons/success_icon.svg',
     color_hex='#00E400',   # green
-    addtional_text='{text}')
+    additional_text='{text}')
 
 failure_html: str = icon_msg_core.format(
     image_rc=':/plugins/citydb_loader/icons/failure_icon.svg',
     color_hex='#FF0000',  # red
-    addtional_text='{text}')
+    additional_text='{text}')
 
 warning_html: str = icon_msg_core.format(
     image_rc=':/plugins/citydb_loader/icons/warning_icon.svg',
-    color_hex='#FFA701',  # Orange
-    addtional_text='{text}')
+    color_hex='#FFA701',  # orange
+    additional_text='{text}')
 
 crit_warning_html: str = icon_msg_core.format(
     image_rc=':/plugins/citydb_loader/icons/critical_warning_icon.svg',
     color_hex='#DA4453', # pale red
-    addtional_text='{text}')
+    additional_text='{text}')
