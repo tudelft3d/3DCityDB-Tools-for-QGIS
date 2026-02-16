98d4c65fbdb9940da0ebc3fabf8e2997e61168be
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2026-02-16 13:44:13 +0100

    Update cdb_tools_main_constants.py

--- cdb_tools_main_constants.py
+++ cdb_tools_main_constants.py
@@ -2,27 +2,30 @@
 """
 import os.path
 
-# Qt Windows Modality enumeration: https://doc.qt.io/qt-5/qt.html#WindowModality-enum
-# Qt.NonModal	        0	The window is not modal and does not block input to other windows.
-# Qt.WindowModal	    1	The window is modal to a single window hierarchy and blocks input to its parent window, all grandparent windows, and all siblings of its parent and grandparent windows.
-# Qt.ApplicationModal	2	The window is modal to the application and blocks input to all windows.
+# Qt Windows Modality enumeration: https://doc.qt.io/qt-6/qt.html#WindowModality-enum
+# from qgis.PyQt.QtCore import Qt
+# Qt.WindowModality.NonModal	        # 0	The window is not modal and does not block input to other windows.
+# Qt.WindowModality.WindowModal	        # 1	The window is modal to a single window hierarchy and blocks input to its parent window, all grandparent windows, and all siblings of its parent and grandparent windows.
+# Qt.WindowModality.ApplicationModal	# 2	The window is modal to the application and blocks input to all windows.
 
 # Supported QGIS minor versions, i.e. version 3.xx
-# QGIS_LTR: tuple = (22,) # With one-item tuples, do not forget to add a column after it!! :-)
-QGIS_LTR: tuple = (22, 28, 34, 40, 44)
+#QGIS3_VERSION_MAJOR: int = 3
+# QGIS_VERSION_MINOR: tuple = (44,) # With one-item tuples, do not forget to add a column after it!! :-)
+QGIS3_VERSION_MINOR: tuple = (40, 44) 
+
 
 # Plugin current version
 PLUGIN_VERSION_MAJOR: int = 0
 PLUGIN_VERSION_MINOR: int = 8
-PLUGIN_VERSION_REV:   int = 12
+PLUGIN_VERSION_REV:   int = 13
 
 # Paths
 PLUGIN_ABS_PATH: str      = os.path.normpath(os.path.dirname(__file__))
-# print("PLUGIN_ABS_PATH", PLUGIN_PATH) # e.g. C:\...\QGIS3\profiles\default\python\plugins\citydb-tools  
+#print("PLUGIN_ABS_PATH", PLUGIN_ABS_PATH) # e.g. C:\...\QGIS3\profiles\default\python\plugins\citydb-tools  
 PLUGIN_ROOT_PATH: str = os.path.split(os.path.dirname(__file__))[0]
-# print("PLUGIN_ROOT_PATH", PLUGIN_ROOT_PATH) # e.g. C:\...\QGIS3\profiles\default\python\plugins
+#print("PLUGIN_ROOT_PATH", PLUGIN_ROOT_PATH) # e.g. C:\...\QGIS3\profiles\default\python\plugins
 PLUGIN_ROOT_DIR: str  = os.path.split(os.path.dirname(__file__))[1]
-# print("PLUGIN_ROOT_DIR", PLUGIN_ROOT_DIR) # e.g. citydb-tools 
+#print("PLUGIN_ROOT_DIR", PLUGIN_ROOT_DIR) # e.g. citydb-tools 
 URL_GITHUB_PLUGIN: str   = "https://github.com/tudelft3d/3DCityDB-Tools-for-QGIS"
 
 # Database schemas where QGIS Package is installed etc.
