1b76b970917df5a5dfe4db57ba731c27fbce7a1f
Giorgio Agugiaro <g.agugiaro@tudelft.nl>
2026-09-07 18:29:18 +0200

    Update tab_conn_functions.py

--- cdb4/gui_deleter/functions/tab_conn_functions.py
+++ cdb4/gui_deleter/functions/tab_conn_functions.py
@@ -296,7 +296,7 @@ def refresh_extents(dlg: CDB4DeleterDialog) -> None:
             # Create new rubber band
             cdb_extents_new_rubber_band: QgsRubberBand = QgsRubberBand(dlg.CANVAS, QgsWkbTypes.PolygonGeometry)
             # cdb_extents_new_rubber_band: QgsRubberBand = QgsRubberBand(mapCanvas=dlg.CANVAS, geometryType=Qgis.GeometryType.Polygon)
-            cdb_extents_new_rubber_band.setLineStyle(penStyle=Qt.PenStype.DashLine)
+            cdb_extents_new_rubber_band.setLineStyle(penStyle=Qt.PenStyle.DashLine)
 
             # Drop the old magenta one
             dlg.RUBBER_DELETE.reset()
